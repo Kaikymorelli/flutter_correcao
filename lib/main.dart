cb811a8b0217e483179934059fb5e10b9d90fea1
Kaikymorelli <132359674+Kaikymorelli@users.noreply.github.com>
2024-05-23 11:38:29 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_correcao/Principal.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp (MaterialApp (
+    debugShowCheckedModeBanner: false,
+    home: Principal()
+));
